1851068bdfcb84f92678947158bfa282bbf038e8
Bryan Haakman <bryanhaakman@gmail.com>
2023-06-21 10:15:43 +0200

    Add API generated by openapi-generator

---
+++ src/apiClient/models/index.ts
@@ -0,0 +1,21 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AdminCic';
+export * from './AdminListCics200Response';
+export * from './BoilerType';
+export * from './CicAvailableWifiNetworksInner';
+export * from './CicStatus';
+export * from './ConnectionStatus';
+export * from './ErrorCode';
+export * from './ErrorResponse';
+export * from './FlowRateTestStatus';
+export * from './HeatDeliverySystem';
+export * from './HeatPump';
+export * from './HeatPumpCommissioning';
+export * from './HeatPumpTestLevel';
+export * from './IdentificationStatus';
+export * from './IdentificationTestStatus';
+export * from './InstallerCicAllOfLastCommissioning';
+export * from './ResponseMeta';
+export * from './SilentMode';
+export * from './ThermostatType';
