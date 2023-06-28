ebf6f9ff7682ddcf5263cdbe170946dc9a46488a
Bryan Haakman <bryanhaakman@gmail.com>
2023-06-28 13:35:00 +0200

    Add an useQueryParams hook

---
+++ src/utils/useQueryParams.ts
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useSearch } from "wouter/use-location";
+
+export const useQueryParams = () => {
+  const location = useSearch()
+  const queryParams = React.useMemo(() => {
+    const urlSearchParams = new URLSearchParams(location);
+    return Object.fromEntries(urlSearchParams);
+  }, [location]);
+
+  return queryParams
+}
