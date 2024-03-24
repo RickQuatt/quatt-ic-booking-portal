c76a6514a762cf760064c8f99540d8122b70443f
safier99 <safier99@gmail.com>
2024-03-24 12:16:21 +0100

    add logo

--- src/cic-detail/CICDetailSectionHeader.tsx
+++ src/cic-detail/CICDetailSectionHeader.tsx
@@ -1,10 +1,24 @@
 import classes from "./CICDetailSectionHeader.module.css";
 
-export function DetailSectionHeader({ title }: { title: string }) {
+export function DetailSectionHeader({
+  title,
+  logo,
+}: {
+  title: string;
+  logo?: string;
+}) {
   return (
-    <div className={classes["detail-section-header"]}>
+    <div
+      className={classes["detail-section-header"]}
+      style={{
+        display: "flex",
+        alignItems: "center",
+        marginBottom: "1rem",
+        borderBottom: "1px solid #737373",
+      }}
+    >
+      {logo && <img src={logo} alt="Logo" style={{ height: "36px" }} />}
       <h3>{title}</h3>
-      <hr />
     </div>
   );
 }
